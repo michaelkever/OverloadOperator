cd61b67abce5619383a73fe56625b5014c979f83
Michael Kever <michaelkever@gmail.com>
2018-07-21 16:32:51 -0500

    Update

--- overloadOperator/Employee.cs
+++ overloadOperator/Employee.cs
@@ -8,39 +8,17 @@ namespace overloadOperator
 {
     class Employee
     {
-        public struct EmployeeId
-        {
-            public int employee01;
-            public int employee02;
+        public int employee01 { get; set; }
+        public int employee02 { get; set; }
 
-            public employeeId(int employee01, int employee02)
+        public static override == (int num01, int num02)
             {
-                this.employee01 = employee01;
-                this.employee02 = employee02;
-            }
-            public static bool operator == (EmployeeId employeeId)
-            {
-                if (employeeId.employee01 == employeeId.employee02)
-                {
-                    return true;
-                }
-                return false;
-            }
 
-            public static bool operator != (EmployeeId employeeId)
-            {
-                return !(employeeId.employee01 == employeeId.employee02);
-            }
+}
+                
 
-            public override bool Equals(object emp)
-            {
-                if (!(emp is EmployeeId))
-                {
-                    return false;
-                }
-                return this == (EmployeeId)emp;
-            }
-        }
+           
+        
     }
 }
 
